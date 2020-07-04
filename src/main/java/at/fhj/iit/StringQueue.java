75fad18fc715762be33b2d7c9ed39100510b0fa7
MarieChristindeCrinis <62705069+MarieChristindeCrinis@users.noreply.github.com>
2020-07-04 14:13:44 +0200

    Adds tests and fixes for the StringQueue

--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -11,10 +11,10 @@ import java.util.NoSuchElementException;
 public class StringQueue implements IQueue {
 
   private List<String> elements = new ArrayList<String>();
-  private int maxSize = 5;
+  private int maxSize;
 
   public StringQueue(int maxsize) {
-    maxSize = maxSize;
+      this.maxSize = maxsize;
   }
 
   @Override
@@ -31,7 +31,7 @@ public class StringQueue implements IQueue {
   public String poll() {
     String element = peek();
 
-    if (elements.size() == 0) {
+    if (element != null ) {
       elements.remove(0);
     }
 
@@ -40,19 +40,21 @@ public class StringQueue implements IQueue {
 
   @Override
   public String remove() {
-    String element = poll();
-    element = "";
+    String element = peek();
     if (element == null)
       throw new NoSuchElementException("there's no element any more");
-
+    else {
+      elements.remove(0);
+    }
     return element;
   }
 
   @Override
   public String peek() {
     String element;
-    if (elements.size() > 0)
-      element = elements.get(0);
+    if (elements.size() > 0) {
+        element = elements.get(0);
+    }
     else
       element = null;
 
--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -5,8 +5,53 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 class StringQueueTest {
 
    // TODO Write your own tests
 
+    @Test
+    public void testStringQueuePool() {
+        StringQueue queue = new StringQueue(5);
+        String testString = "Nr. 1";
+        queue.offer("Nr. 1");
+        String test = queue.poll();
+        assertEquals(testString, test);
+    }
+
+    @Test
+    public void testStringQueueRemove() {
+        StringQueue queue = new StringQueue(1);
+        queue.offer("test");
+        String test = queue.remove();
+        assertEquals("test", test);
+        try {
+            queue.remove();
+        } catch(NoSuchElementException ex) {
+            assertEquals("there's no element any more", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testStringQueueElement() {
+        StringQueue queue = new StringQueue(1);
+        try{
+            queue.element();
+        } catch (NoSuchElementException ex) {
+            assertEquals("there's no element any more", ex.getMessage());
+        }
+        queue.offer("test");
+        String test = queue.element();
+        assertEquals("test", test);
+    }
+
+    @Test
+    public void testStringQueueOffer() {
+        StringQueue queue = new StringQueue(1);
+        boolean check = queue.offer("blabla");
+        assertEquals(true,check);
+        check = queue.offer("yikes");
+        assertEquals(false,check);
+    }
 }
